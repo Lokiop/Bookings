681b1c5fb625c4dd5de1e55ff266efc042b7d78c
Darshan <darshanheda08@gmail.com>
2023-01-25 19:26:57 +0530

    driver for using postgres

---
+++ internal/driver/driver.go
@@ -0,0 +1,72 @@
+package driver
+
+import (
+	"database/sql"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v5"
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+// DB holds the databse connection pool
+type DB struct {
+	SQL *sql.DB
+}
+
+var dbConn = &DB{}
+
+const maxOpenDbConn = 10
+
+const maxIdleDbConn = 5
+
+const maxDbLifeTime = 5 * time.Minute
+
+// ConnectSQL creates database pool for Postgres
+func ConnectSQL(dsn string) (*DB, error) {
+	d, err := NewDatabase(dsn)
+
+	if err != nil {
+		panic(err)
+	}
+
+	d.SetMaxOpenConns(maxOpenDbConn)
+	d.SetMaxIdleConns(maxIdleDbConn)
+	d.SetConnMaxLifetime(maxDbLifeTime)
+
+	dbConn.SQL = d
+
+	err = testDB(d)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return dbConn, nil
+}
+
+// testDB tries to ping the database
+func testDB(d *sql.DB) error {
+	err := d.Ping()
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// NewDatabase creates a new Databse for the application
+func NewDatabase(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
